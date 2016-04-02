60fbb8ab06218ee88dde67608221f1e1555b4612
Julian Lopez <julian.lopezv@udea.edu.co>
2016-04-02 11:53:06 -0500

    Components from the App (Controllers and Views)

---
+++ app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import {Hero} from './hero';
+
+export var Heroes:Hero[]=[
+	{"id":11, "name":"Ironman"},
+	{"id":11, "name":"Thor"},
+	{"id":11, "name":"Captain America"},
+	{"id":11, "name":"Ant Man"},
+	{"id":11, "name":"Deadpool"},
+	{"id":11, "name":"Spiderman"},
+	{"id":11, "name":"Wolverine"},
+	{"id":11, "name":"Hulk"},
+	{"id":11, "name":"Black Widow"},
+	{"id":11, "name":"Daredevil"}
+];
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
